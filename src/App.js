7af6881bb0bae7c489b6b149d2b02d59c0c74464
PL-Kenshin <78429627+PL-Kenshin@users.noreply.github.com>
2022-05-06 14:39:08 +0200

    first commit via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import Posts from "./components/posts";
+
+function App() {
+  return (
+      <main className="container">
+          <Posts/>
+      </main>
+  );
+}
+
+export default App;
